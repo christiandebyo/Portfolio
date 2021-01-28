afc28f2baac93cb8eaf50e1eba0a8a6f77dbffa8
christiandebyo <33336124+christiandebyo@users.noreply.github.com>
2021-01-28 23:24:21 +0800

    added templates folder and backend for article

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/article')
+def article():
+    return render_template('article.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)s
