da23e1e57fece54484efaff989189f2ed9514042
christiandebyo <33336124+christiandebyo@users.noreply.github.com>
2021-01-30 20:54:09 +0800

    dummy database set

--- app.py
+++ app.py
@@ -2,10 +2,19 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+    'title': 'Header One',
+    'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit veritatis officiis officia at commodi magni quidem quaerat enim libero sed rem, nisi recusandae similique non ipsam praesentium quasi mollitia eos eligendi, odio vitae animi incidunt cumque. Distinctio ut voluptatibus error officia provident aliquam similique consectetur animi possimus. Harum, voluptatibus enim?',
+    'author': 'Debyo Santos',
+    'date_created': 'January 1, 2021'
+    }
+]
+
 @app.route('/')
 @app.route('/home')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
 
 @app.route('/article')
 def article():
