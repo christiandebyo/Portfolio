13ac5f1bb5707801bc4daef19be54a0ee36365c5
christiandebyo <33336124+christiandebyo@users.noreply.github.com>
2021-01-31 20:52:07 +0800

    initiated temp db in sqlite

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # temp db details here (sqlite)
+db = SQLAlchemy(app) # instance of db
+
 
 posts = [
     {
